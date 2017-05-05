42c898cbc3c2baf7fead3bdf3d09dbaffe64cc63
Firdevs Kürkçü <Firdevs Krk>
2017-05-05 18:48:04 +0200

    Aufgaben 1 und 2 erledigt

---
+++ aufgabe2.js
@@ -0,0 +1,26 @@
+var fs = require ('fs');
+var chalk = require ('chalk');
+
+var colors = ['cyan', 'gray', 'magenta']
+var colorRotator = 0;
+
+function rotateColor() {
+  var rotation = colorRotator++;
+  if (colorRotator >= colors.length) {
+    colorRotator = 0;
+  }
+  return colors[rotation];
+}
+
+
+fs.readFile(__dirname + "/staedte.json", function (err, data) {
+  JSON.parse(data).cities.forEach(function(city) {
+  for (var attribute in city)  {
+    if (city.hasOwnProperty(attribute)) {
+      console.log(attribute + ': '+ city[attribute]);
+    }
+  }
+  console.log('------------------------------');
+
+});
+});
